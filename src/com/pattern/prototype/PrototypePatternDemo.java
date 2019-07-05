6c7cfe1417209bac79e9fca6d9373d50bcded5df
Jose Luis Rodriguez Araujo <ojuarass@gmail.com>
2019-07-05 12:30:27 -0500

    [ADD] Prototype pattern

---
+++ src/com/pattern/prototype/PrototypePatternDemo.java
@@ -0,0 +1,17 @@
+package com.pattern.prototype;
+
+public class PrototypePatternDemo {
+
+	public static void main(String[] args) {
+		ShapeCache.loadCache();
+		
+		Shape clonedShape1 = ShapeCache.getShape("1");
+		System.out.println("Shape : " + clonedShape1.getType());
+		Shape clonedShape2 = ShapeCache.getShape("2");
+		System.out.println("Shape : " + clonedShape2.getType());
+		Shape clonedShape3 = ShapeCache.getShape("3");
+		System.out.println("Shape : " + clonedShape3.getType());
+		
+	}
+
+}
