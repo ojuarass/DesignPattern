25d8ca3b110e96d2c86047c6f84b8df90735ca17
Jose Luis Rodriguez Araujo <ojuarass@gmail.com>
2019-07-08 15:52:24 -0500

    [ADD] Bridge pattern

---
+++ src/com/pattern/bridge/Shape.java
@@ -0,0 +1,11 @@
+package com.pattern.bridge;
+
+public abstract class Shape {
+	protected DrawAPI drawAPI;
+	
+	protected Shape(DrawAPI drawAPI) {
+		this.drawAPI = drawAPI;
+	}
+	
+	public abstract void draw();
+}
