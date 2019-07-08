50fe90f65c4e3c4821d29df7a554f36ca7633e00
Jose Luis Rodriguez Araujo <ojuarass@gmail.com>
2019-07-08 15:33:19 -0500

    [ADD] Adapter pattern

---
+++ src/com/pattern/adapter/Mp4Player.java
@@ -0,0 +1,15 @@
+package com.pattern.adapter;
+
+public class Mp4Player implements AdvancedMediaPlayer {
+
+	@Override
+	public void playVlc(String fileName) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void playMp4(String fileName) {
+		System.out.println("Playing mp4 file. Name: "+ fileName);	
+	}
+
+}
