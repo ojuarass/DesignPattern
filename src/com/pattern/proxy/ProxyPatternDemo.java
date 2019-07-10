ddcb374d0db64e71cd310f6b707bf862094172f0
Jose Luis Rodriguez <ojuarass@gmail.com>
2019-07-09 19:29:52 -0500

    [ADD] Proxy pattern

---
+++ src/com/pattern/proxy/ProxyPatternDemo.java
@@ -0,0 +1,14 @@
+package com.pattern.proxy;
+
+public class ProxyPatternDemo {
+
+	public static void main(String[] args) {
+		Image image = new ProxyImage("test_10mb.jpg");
+		
+		image.display();
+		System.out.println();
+		
+		image.display();
+	}
+
+}
