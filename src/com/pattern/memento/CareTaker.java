3796cffc9eded013ca03aa1b02236e5c997343aa
Jose Luis Rodriguez <ojuarass@gmail.com>
2019-07-25 12:44:59 -0500

    [ADD] Memento pattern

---
+++ src/com/pattern/memento/CareTaker.java
@@ -0,0 +1,16 @@
+package com.pattern.memento;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CareTaker {
+	private List<Memento> mementoList = new ArrayList<Memento>();
+	
+	public void add(Memento memento) {
+		mementoList.add(memento);
+	}
+	
+	public Memento get(int index) {
+		return mementoList.get(index);
+	}
+}
