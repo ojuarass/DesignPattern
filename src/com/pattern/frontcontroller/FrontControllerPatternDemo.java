35a4f31ae6a098a15651f29c673f1df8fc1753f6
Jose Luis Rodriguez <ojuarass@gmail.com>
2019-07-29 17:27:23 -0500

    [ADD] Front Request pattern

---
+++ src/com/pattern/frontcontroller/FrontControllerPatternDemo.java
@@ -0,0 +1,11 @@
+package com.pattern.frontcontroller;
+
+public class FrontControllerPatternDemo {
+
+	public static void main(String[] args) {
+		FrontController frontController = new FrontController();
+		frontController.dispatchRequest("Home");
+		frontController.dispatchRequest("Student");
+	}
+
+}
---
+++ src/com/pattern/frontcontroller/StudentView.java
@@ -0,0 +1,7 @@
+package com.pattern.frontcontroller;
+
+public class StudentView {
+	public void show() {
+		System.out.println("Displaying Student Page.");
+	}
+}
