a52c8b54fb48289ba172f2b2d77dbbd7a14356dd
Jose Luis Rodriguez <ojuarass@gmail.com>
2019-07-29 13:46:36 -0500

    [ADD] Visitor pattern

---
+++ src/com/pattern/visitor/VisitorPatternDemo.java
@@ -0,0 +1,9 @@
+package com.pattern.visitor;
+
+public class VisitorPatternDemo {
+
+	public static void main(String[] args) {
+		ComputerPart computerPart = new Computer();
+		computerPart.accept(new ComputerPartDisplayVisitor());
+	}
+}
