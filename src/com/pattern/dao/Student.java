f61ec89e7afdda470bba5c0918a36db771999195
Jose Luis Rodriguez <ojuarass@gmail.com>
2019-07-29 17:12:53 -0500

    [ADD] Dao pattern

---
+++ src/com/pattern/dao/Student.java
@@ -0,0 +1,25 @@
+package com.pattern.dao;
+
+public class Student {
+	private int rollNo;
+	private String name;
+	
+	public Student(String name, int rollNo) {
+		this.rollNo = rollNo;
+		this.name = name;
+	}
+	public int getRollNo() {
+		return rollNo;
+	}
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
