568f9fd56d4c599a8ae1f758b3b9d9a753aa11ad
Jose Luis Rodriguez <ojuarass@gmail.com>
2019-07-31 10:27:34 -0500

    [ADD] Service Locator pattern

---
+++ src/com/pattern/servicelocator/Cache.java
@@ -0,0 +1,34 @@
+package com.pattern.servicelocator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cache {
+	private List<Service> services;
+	
+	public Cache() {
+		this.services = new ArrayList<Service>();
+	}
+	
+	public Service getService(String serviceName) {
+		for (Service service : services) {
+			if(service.getName().equalsIgnoreCase(serviceName)){
+	            System.out.println("Returning cached  " + serviceName + " object");
+	            return service;
+	         }
+		}
+		return null;
+	}
+	
+	public void addService(Service newService) {
+		boolean exist = false;
+		for (Service service : services) {
+			if (service.getName().equalsIgnoreCase(newService.getName())) {
+				exist = true;
+			}
+		}
+		if (!exist) {
+			services.add(newService);
+		}
+	}
+}
---
+++ src/com/pattern/servicelocator/Service.java
@@ -0,0 +1,6 @@
+package com.pattern.servicelocator;
+
+public interface Service {
+	public String getName();
+	public void execute();
+}
