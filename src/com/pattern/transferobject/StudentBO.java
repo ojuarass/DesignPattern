cb4f3dfe853244e88aba9a210c9dfee937dea9a8
Jose Luis Rodriguez <ojuarass@gmail.com>
2019-07-31 10:54:54 -0500

    [ADD] Transfer Object pattern

---
+++ src/com/pattern/transferobject/StudentBO.java
@@ -0,0 +1,32 @@
+package com.pattern.transferobject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentBO {
+	List<StudentVO> students;
+	
+	public StudentBO() {
+		students = new ArrayList<StudentVO>();
+		students.add(new StudentVO("Robert", 0));
+		students.add(new StudentVO("John", 1));
+	}
+	
+	public void deleteStudent(StudentVO student) {
+		students.remove(student.getRollNo());
+		System.out.println("Student: Roll No " + student.getRollNo() + ", deleted from database");
+	}
+	
+	public List<StudentVO> getAllStudents(){
+		return students;
+	}
+	
+	public StudentVO getStudent(int rollNo) {
+		return students.get(rollNo);
+	}
+	
+	public void updateStudent(StudentVO student) {
+		students.get(student.getRollNo()).setName(student.getName());
+		System.out.println("Student: Roll No " + student.getRollNo() +", updated in the database");
+	}
+}
